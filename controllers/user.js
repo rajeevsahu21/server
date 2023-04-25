a983ac38b4628f3e255f04f12f54d81b96792bcf
Rajeev Sahu <rajeevsahu210@gmail.com>
2023-04-25 22:59:06 +0530

    fixed user

--- controllers/user.js
+++ controllers/user.js
@@ -30,7 +30,7 @@ const getAllMaleUser = async (req, res) => {
   try {
     const users = await User.find({
       gender: "Male",
-      phone_price: { $gt: 1000 },
+      phone_price: { $gt: 10000 },
     });
     res.status(200).json({ message: "Data Found Successfully", data: users });
   } catch (err) {
@@ -39,4 +39,59 @@ const getAllMaleUser = async (req, res) => {
   }
 };
 
-export { importUsers, getAllusersHavingIncomeLessThan5, getAllMaleUser };
+const getAllUsersQuote = async (req, res) => {
+  try {
+    const users = await User.find({
+      last_name: { $regex: /^M.*/ },
+      quote: { $exists: true },
+      $expr: { $gt: [{ $strLenCP: "$quote" }, 15] },
+      email: { $regex: (/.*$last_name.*/, "i") },
+    });
+    res.status(200).json({ message: "Data Found Successfully", data: users });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+const getAllUserWhoseEmailNotIncludeAnyDigit = async (req, res) => {
+  try {
+    const users = await User.find({
+      $or: [{ car: "BMW" }, { car: "Mercedes" }, { car: "Audi" }],
+      email: { $regex: /^[^\d\s@]+@[^\d\s@]+\.[^\d\s@]+$/ },
+    });
+    res.status(200).json({ message: "Data Found Succesfully", data: users });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+const getTop10CityData = async (req, res) => {
+  try {
+    const users = await User.aggregate([
+      {
+        $group: {
+          _id: "$city",
+          count: { $sum: 1 },
+          avg_income: { $avg: "$income" },
+        },
+      },
+      { $sort: { count: -1 } },
+      { $limit: 10 },
+    ]);
+    res.status(200).json({ message: "Data Found SuccessFully", data: users });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export {
+  importUsers,
+  getAllusersHavingIncomeLessThan5,
+  getAllMaleUser,
+  getAllUsersQuote,
+  getAllUserWhoseEmailNotIncludeAnyDigit,
+  getTop10CityData,
+};
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,18 @@
 import { Router } from "express";
 import {
   getAllMaleUser,
+  getAllUserWhoseEmailNotIncludeAnyDigit,
+  getAllUsersQuote,
   getAllusersHavingIncomeLessThan5,
+  getTop10CityData,
 } from "../controllers/user.js";
 
 const router = Router();
 
 router.get("/income", getAllusersHavingIncomeLessThan5);
 router.get("/male", getAllMaleUser);
+router.get("/quote", getAllUsersQuote);
+router.get("/email", getAllUserWhoseEmailNotIncludeAnyDigit);
+router.get("/city", getTop10CityData);
 
 export default router;
